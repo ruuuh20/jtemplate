bdc0eec13d460f12a81b341e772e922f9e680ce3
ruuuh20 <pkim2320@gmail.com>
2022-10-01 12:57:30 -0400

    first commit

---
+++ webpack.config.build.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+
+const { merge } = require('webpack-merge')
+const config = require('./webpack.config')
+
+
+module.exports = merge(config, {
+  mode: 'production',
+
+  output: {
+    path: path.join(__dirname, 'public')
+  },
+ 
+})
